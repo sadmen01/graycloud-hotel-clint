0db19f1da8f4d67b81d58711da09ee46b91f12aa
sadmen rashik khan <sadmenrashik06@gmail.com>
2022-10-19 17:22:23 +0600

    log in anf register from

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Form } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            
-        </div>
+        <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="Text" placeholder="Name" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+  
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="outline-success" type="submit">
+          Register
+        </Button>
+      </Form>
     );
 };
 
